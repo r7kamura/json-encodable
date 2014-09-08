e9048d8e38f8af1a8a6acc64a00a0e47cc745d4a
Ryo Nakamura <r7kamura@gmail.com>
2014-09-08 23:29:26 +0900

    v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.2
+* Fix loading logic for bundler :bug:
+
+## 0.0.1
+* 1st Release :octocat:
--- lib/json/encodable/version.rb
+++ lib/json/encodable/version.rb
@@ -1,5 +1,5 @@
 module JSON
   module Encodable
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
